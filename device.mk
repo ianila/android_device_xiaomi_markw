a35414efb9eaf809ff4097023fa8c84930e4a5ee
Razziell <razziell@ya.ru>
2018-05-05 19:50:23 +0300

    markw: use enforce rro targets

--- device.mk
+++ device.mk
@@ -28,6 +28,13 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    Bluetooth \
+    Settings \
+    SettingsProvider \
+    SystemUI \
+    framework-res \
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
